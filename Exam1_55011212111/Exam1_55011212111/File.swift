6715f0281a834d627857fbe54171dd1b48eda26e
55011212018ark <imac@msu>
2014-10-10 15:57:48 +0700

    midterm_55011212111

---
+++ Exam1_55011212111/Exam1_55011212111/File.swift
@@ -0,0 +1,18 @@
+//
+//  File.swift
+//  Exam1_55011212111
+//
+//  Created by student on 10/10/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import Foundation
+class File_2  {
+    var total1:Double
+    var volume:Double
+    var price:Double{
+    get{
+        return volume * self.price
+         }
+    }
+}
