cd942069ce157d9fc0845b60078f4bf2989100a1
55011212018ark <imac@msu>
2014-10-31 16:41:33 +0700

    31/10/57

---
+++ Exam1_55011212111/Exam1_55011212111/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Exam1_55011212111
+//
+//  Created by student on 10/10/14.
+//  Copyright (c) 2014 student. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var textName: UITextField!
+    @IBOutlet var textVolume: UITextField!
+    @IBOutlet var textPrice: UITextField!
+    @IBOutlet var buttonTotal: UIButton!
+    @IBOutlet var textTotal: UITextField!
+    @IBOutlet var buttonProfit: UIButton!
+    @IBOutlet var tabelView: UITableView!
+    
+    
+
+    
+      override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
