6293ce572f66cb3e85244e0dcae86e9b4d2a854f
55011212018ark <imac@msu>
2014-12-17 13:00:53 +0700

    final

--- Exam1_55011212111/Exam1_55011212111/ViewController.swift
+++ Exam1_55011212111/Exam1_55011212111/ViewController.swift
@@ -2,29 +2,55 @@
 //  ViewController.swift
 //  Exam1_55011212111
 //
-//  Created by student on 10/10/14.
+//  Created by student on 12/17/14.
 //  Copyright (c) 2014 student. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController ,UITableViewDataSource{
     
-    @IBOutlet var textName: UITextField!
-    @IBOutlet var textVolume: UITextField!
-    @IBOutlet var textPrice: UITextField!
-    @IBOutlet var buttonTotal: UIButton!
-    @IBOutlet var textTotal: UITextField!
-    @IBOutlet var buttonProfit: UIButton!
-    @IBOutlet var tabelView: UITableView!
+   var items = [String]()
     
     
-
+    @IBOutlet weak var tableView: UITableView!
     
-      override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+    func tableView(tableview:UITableView,numberOfRowsInSection section:Int)->Int{
+        return items.count
     }
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let Cell = tableView.dequeueReusableHeaderFooterViewWithIdentifier ("Cell") as UITableViewCell
+        Cell.textLabel!.text = items[indexPath.row]
+        return Cell
+        
+    }
+   
+    @IBAction func addItems(sender: AnyObject) {
+        var alert = UIAlertController(title: "New items", message: "Add a new Name", preferredStyle: .Alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .Default){(action:UIAlertAction!)->Void in
+            
+            let textField = alert.textFields![0] as UITextField
+            self.items.append(textField.text)
+            self.tableView.reloadData()
+        }
+        let cancelAction = UIAlertAction(title: "Cancel", style: .Default){(ACTION : UIAlertAction!) -> Void in
+        }
+        alert.addTextFieldWithConfigurationHandler{(textField: UITextField!)-> Void in
+        }
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        presentViewController(alert,animated: true,completion: nil)
+
+    }
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "\"Stocks\""
+        tableView.registerClass(UITableViewCell.self,forCellReuseIdentifier:"Cell")
+
+           }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
